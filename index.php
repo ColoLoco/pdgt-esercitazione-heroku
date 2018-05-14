60f9cd9e40113ba8db571945e6a62a3b25cb1a75
Giacomo Colonesi <g.colonesi@gmail.com>
2018-05-14 10:38:48 +0200

    Modifica codice

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Hello world <?php echo date ("Y"); ?>!
+<?php
+$content = file_get_contents("php://input");
+
+if (!$content)
+  exit();
+
+$update = json_decode($content, false);
+$message = $update->message;
+
+$message_id = $message->message_id;
+$text = $message->text;
+
+error_log("Message ID {$message_id}: {$text}\n");
