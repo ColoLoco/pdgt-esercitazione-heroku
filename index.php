aba667be53ed8ac2610ca000a12e69b24a4bb108
Giacomo Colonesi <g.colonesi@gmail.com>
2018-05-14 11:15:55 +0200

    Modfica file index.php con aggiunta BOTTOKEN

--- index.php
+++ index.php
@@ -11,3 +11,16 @@ $message_id = $message->message_id;
 $text = $message->text;
 
 error_log("Message ID {$message_id}: {$text}\n");
+
+$chat_id = $message->chat->id;
+$token = getenv("BOTTOKEN");  // prendiamo il token del bot da "heroku tramite la funz. "getenv"
+
+$url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" .
+urlencode("Ciao, mi hai scritto: {$text}");
+
+$handle = curl_init($url);
+curl_septopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_septopt($handle, CURLOPT_POST, true);
+$response = curl_exec($handle);
+
+error_log("sendMessage: " . response);
